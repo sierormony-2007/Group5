3d98db0de32441fcc9f57174f682434c6cc22939
Sie Rormony <niin2015@gmail.com>
2025-11-11 10:58:36 +0700

    push

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -2,10 +2,27 @@
 #include <iostream>
 #include "stack.hpp"
 using namespace std;
-stack<char> temp;
+
+
+// stack<char> temp;
+// int main(){
+// string s = "{}[]{}";
+// isValid(s);
+// cout << (isValid(s) ? "Valid" : "Invalid") << endl;
+//   return 0;
+// }
+
 int main(){
-string s = "(}[]{}";
-isValid(s);
-cout << (isValid(s) ? "Valid" : "Invalid") << endl;
-  return 0;
-}
+    Stack s;
+    bool isBalance(){
+      for(int i=0; i<s.length(); i++){
+        if(s.[i]=='(' || s.[i]=='{' || s.[i]=='['){
+          s[i].push();
+        }else if(s.[i]=='(' || s.[i]=='{' || s.[i]=='['){
+
+        }
+
+        }
+      }
+    }
+  
--- Stack/stack.hpp
+++ Stack/stack.hpp
@@ -1,23 +1,58 @@
 #include <stack>
 #include <iostream>
 using namespace std;
-bool isValid(string s) {
-stack<char> temp;
-for (char c : s) {
-    if (c == '(' || c == '{' || c == '[') {
-        temp.push(c);
-    } else {
-        if (temp.empty()) return false;
-        char top = temp.top();
-        if ((c == ')' && top != '(') ||
-            (c == '}' && top != '{') ||
-            (c == ']' && top != '[')) {
-            return false;
-        }
-        temp.pop();
-    }  
-}
-return temp.empty(); 
-
-
+//balance bracket using stack implementation
+struct Node
+{
+    /* data */
+    char data;
+    Node* next;
 };
+
+class Stack{
+    public:
+    Node* top;
+    void push(char c){
+        Node* newNode = new Node();
+        newNode->data = c;
+        newNode->next = top;
+        top = newNode;
+    }
+    Node* pop(){
+        if(top == nullptr) return;
+        Node* temp = top;
+        top = top->next;
+        return temp;
+    }
+    char peek(){
+        if(top == nullptr) return '\0';
+        return top->data;
+    }
+    bool isEmpty(){
+        return top == nullptr;
+    }
+
+   
+};
+
+
+// bol isValid(string s) {
+
+    
+// // stack<char> temp;
+// // for (char c : s) {
+// //     if (c == '(' || c == '{' || c == '[') {
+// //         temp.push(c);
+// //     } else {
+// //         if (temp.empty()) return false;
+// //         char top = temp.top();
+// //         if ((c == ')' && top != '(') ||
+// //             (c == '}' && top != '{') ||
+// //             (c == ']' && top != '[')) {
+// //             return false;
+// //         }
+// //         temp.pop();
+// //     }  
+// // }
+// // return temp.empty(); 
+// };
